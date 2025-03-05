a7494108a8f54521633e64ae7f7d95e4dd5f2bab
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-05 20:17:13 +0000

    box A and a

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	int ch = 97;
+	 int A = 65;
+
+	while (ch <= 122) {
+		putchar(ch);
+			ch++;
+	}
+
+    while (A <= 90) {
+	    putchar(A);
+            A++;
+    }			    ;
+	    putchar('\n');
+	    return 0;
+}	    
