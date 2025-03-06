c78b10e4b34e970c6c19798806dca6d90c94433e
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-06 09:45:06 +0000

    0-f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+     int main(){
+	     int ch = 48;
+             int a = 97;
+	     while (ch <= 57){
+	      
+		      putchar(ch);
+		      ch++;
+	      }
+              while (a <= 102 ){
+                  putchar(a);
+		  a++;
+	      }
+	      putchar('\n');
+	      return 0;
+     }
+
