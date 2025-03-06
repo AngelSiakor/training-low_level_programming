2c0daa6706f28a9d0b99853fbe23ba51c5b01639
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-06 16:32:06 +0000

    z-a Z-A

---
+++ 0x01-variables_if_else_while/2-print_alphabets_reverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+  int main() {
+	  int z = 122;
+	  int Z = 90;
+	  while(97 <=z ) {
+		  putchar(z);
+		  z--;
+	 }
+
+          while(65 <=Z ) {
+		  putchar(Z);
+		  Z--;
+        }
+	  putchar('\n');
+	  return 0;
+  }
+
