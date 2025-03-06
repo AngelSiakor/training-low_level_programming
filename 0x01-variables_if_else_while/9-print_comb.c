9a1c724ad5b90348c66f4167a104258b55b01af7
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-06 14:23:06 +0000

    comma after #

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
   int main(){
-	  int num =48,;
-          
-	}
+	  int p = 0;
+      
+	 while (p < 10) {
+             putchar('0' + p);
+
+           if (p < 9) {
+
+         putchar(',');
+         putchar(' ');
+   }     
+       p++;
+
+   }
+    putchar('\n');	 
+      return 0;
+}
+
