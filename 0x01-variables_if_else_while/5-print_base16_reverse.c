a7cec9abc254b2426cfe33cdb8a7916028009734
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-06 23:34:04 +0000

    reverse in base 16

---
+++ 0x01-variables_if_else_while/5-print_base16_reverse.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+	int f = 102;
+	int p = 57;
+	while(97 <= f)
+     {
+	     putchar(f);
+	       f--;
+     }
+	while(48 <=p)
+       	{
+		putchar(p);
+		p--;
+	}	
+        putchar('\n');
+         return 0 ;	
+}
