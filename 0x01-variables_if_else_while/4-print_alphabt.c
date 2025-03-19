22199129dc3d1f304fe5df630d4ce0d8283f0704
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-19 18:32:19 +0000

    skip e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,15 +4,16 @@ int main (void)
 	int a = 97;
 
 	while(a <= 122)
-{
+	{
 	if (a == 101 || a== 113)
-{
-	 a++;
-	 continue;
-}
-	putchar(a);
-	a++;
-}
+	{
+		 a++;
+		 continue;
+	}
+		putchar(a);
+		a++;
+	}
+	
 	putchar('\n');
 
 	return (0);
