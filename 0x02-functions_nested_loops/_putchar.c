07c90326969f5cb9255baf7b670c62d3db1f815e
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-20 13:33:12 +0000

    print _putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a single character
+ * @c: the character to print
+ *
+ * Return: 1 for success, -1 for error.
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
