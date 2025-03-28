eaae50edec280ba62e322bf472be1ee4efcb0773
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-28 14:00:49 +0000

    prints the alphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,3 +1,21 @@
 #include "main.h"
 
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Return: return 0
+ */
+
+void print_alphabet(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
+
 
