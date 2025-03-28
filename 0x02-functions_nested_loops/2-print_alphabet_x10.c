da790a8084383bb55e3c99a2078d90f5a501333d
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-28 16:06:47 +0000

    print the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int a;
+	int i; 
+
+	for (i= 0; i < 10; i++)
+	{
+	 for (a= 97; a <= 122; a++)
+	 {
+		 _putchar(a);
+	 }
+       
+	_putchar('\n');
+	}
+}
