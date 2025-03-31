ece9287aeb713157cac76e902730a4bc494bf1c4
Angel Siakor <princessangelvahn1@gmail.com>
2025-03-31 15:26:22 +0000

    print letters

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,15 @@
 #include "main.h"
 
+/**
+ * _isalpha - prints lower and uppercase letter
+ * @c: the integer to check
+ *
+ * Return: return 1 if it's a letter and 0 if otherwise
+ */
+
 int _isalpha(int c)
 {
-	
-	if((c < 'A' ||  c >= 'Z')&& (c < 'a' || c > 'z'))
+	if ((c < 'A' ||  c >= 'Z') && (c < 'a' || c > 'z'))
 	{
 		return (0);
 	}
@@ -14,6 +20,6 @@ int _isalpha(int c)
 	}
 
 		return (0);
-} 
+}
 
 
