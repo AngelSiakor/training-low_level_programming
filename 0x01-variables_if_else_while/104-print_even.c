4d2911847db521c061c41a6bf113fa38ec1193bb
Angel Siakor <princessangelvahn1@gmail.com>
2025-04-17 16:40:36 +0000

    even number

--- 0x01-variables_if_else_while/104-print_even.c
+++ 0x01-variables_if_else_while/104-print_even.c
@@ -9,18 +9,28 @@ int main()
 	{	
 		if (a % 2 == 0)
 		{
-
-		      if (a >= 10)
+		       	if (a >= 10 && a <= 99)
 			{
 				int first = a / 10;
 				int last = a % 10;
 				putchar(first + 48);
 				putchar(last + 48);
 			}
-			else 
+			else if (a < 10)
 				putchar(a + 48);
-				putchar(',');
+			else 
+			{
+				putchar((a / 100) + 48);
+				putchar(((a / 10) % 10) + 48);
+				putchar((a % 10) + 48);
+			}
+
+			if (a < 100)
+			{
 				putchar(' ');
+				putchar(',');
+			}
+
 				
 		}
 		a++;
