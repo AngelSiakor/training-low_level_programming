319fe979f7b31bc4e0b5e89a5a35cd8691ab3cf8
Angel Siakor <princessangelvahn1@gmail.com>
2025-04-29 11:09:35 +0000

    even number

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+
+void print_array(int *a, int n)
+{
+
+	a[0] = n[0];
+	a[1] = n[1];
+	a[2] = n[2];
+	a[3] = n[3];
+	a[4] = n[4];
+
+	printf("%d, %d, %d, %d, %d\n", n , n , n , n , n);
+}
