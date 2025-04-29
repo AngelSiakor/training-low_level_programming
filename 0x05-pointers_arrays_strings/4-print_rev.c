54123cad7dbd72a701097c4286e18f995bb690a5
Angel Siakor <princessangelvahn1@gmail.com>
2025-04-29 09:30:02 +0000

    print numbers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+void print_rev(char *s)
+{
+	int length = strlen(s);
+	int middle = length / 2;
+	char temp;
+	int i;
+
+	for (i = 0; i < middle; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i -1];
+		s[length - i - 1] = temp;
+	}
+}
