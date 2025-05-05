c85b603e166b2df0d733d65d2d2526391036f9ab
Angel Siakor <princessangelvahn1@gmail.com>
2025-05-05 17:31:36 +0000

    switch a by 4 without using switch

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+char *leet(char *str)
+{
+	int i;
+	int a;
+	char s[10] ="aAeEoOtTlL";
+	char p[10] ="4433007711";
+	for(i = 0; str[i] != '\0'; i++)
+	{
+		for(a= 0; a < 10; a++)
+		{
+			if (str[i] == s[a])
+				str[i] = p[a];
+		}
+	}
+
+	return(str);
+}
+		
