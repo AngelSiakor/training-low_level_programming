7b50dd47a8f4609d8144eaa96773422b003f0af3
Angel Siakor <princessangelvahn1@gmail.com>
2025-05-06 10:03:56 +0000

    encode a to the 13th place

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+char *rot13(char *str)
+{
+    int i;
+    int a;
+    char s[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char p[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        for (a = 0; a <= 52; a++)
+        {
+            if (str[i] == s[a])
+		    str[i] = p[a];
+        }
+    }
+
+    return (str);
+}
