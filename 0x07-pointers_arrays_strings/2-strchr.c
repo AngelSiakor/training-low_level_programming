08b9f63e9b53bec1d210126457da033269fe336a
Angel Siakor <princessangelvahn1@gmail.com>
2025-05-07 17:55:13 +0000

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - fills memory with abyte
+ * @s: the string to check
+ * @c: the character
+ *
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (c == '\0')
+		return (s);
+	else
+		return (NULL);
+}
+
