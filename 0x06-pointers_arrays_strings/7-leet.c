aac5360b908feb51ac4a64bbe02ec99d9c4a4263
Angel Siakor <princessangelvahn1@gmail.com>
2025-05-07 16:49:51 +0000

    encodes a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,21 +2,28 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * leet - encodes a string
+ * @str: the string to check
+ *
+ * Return: str
+ */
+
 char *leet(char *str)
 {
 	int i;
 	int a;
-	char s[10] ="aAeEoOtTlL";
-	char p[10] ="4433007711";
-	for(i = 0; str[i] != '\0'; i++)
+	char s[10] = "aAeEoOtTlL";
+	char p[10] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for(a= 0; a < 10; a++)
+		for (a = 0; a < 10; a++)
 		{
 			if (str[i] == s[a])
 				str[i] = p[a];
 		}
 	}
 
-	return(str);
+	return (str);
 }
-		
