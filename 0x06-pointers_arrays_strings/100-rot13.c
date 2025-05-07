958860e1b2013b8e8827c95437c57d36ad27b8ff
Angel Siakor <princessangelvahn1@gmail.com>
2025-05-07 17:08:47 +0000

    encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,21 +2,27 @@
 #include "main.h"
 #include <string.h>
 
+/**
+ * rot13 - encodes a string using rot13
+ * @str: the string to check
+ *
+ * Return: str
+ */
+
 char *rot13(char *str)
 {
-    int i;
-    int a;
-    char s[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char p[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i;
+	int a;
+	char s[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	char p[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (a = 0; a <= 52; a++)
-        {
-            if (str[i] == s[a])
-		    str[i] = p[a];
-        }
-    }
-
-    return (str);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (a = 0; a <= 52; a++)
+		{
+			if (str[i] == s[a])
+				str[i] = p[a];
+		}
+	}
+	return (str);
 }
